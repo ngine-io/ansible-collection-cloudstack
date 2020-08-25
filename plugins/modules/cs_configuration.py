0ec47e6da2e9fc32d2128affb50e3e6ead2b8b65
David Passante <david.passante@orange.com>
2020-08-25 16:15:06 +0200

    Workaround for empty global settings idempotency (#25)

--- plugins/modules/cs_configuration.py
+++ plugins/modules/cs_configuration.py
@@ -224,7 +224,8 @@ class AnsibleCloudStackConfiguration(AnsibleCloudStack):
         configuration = self.get_configuration()
         args = self._get_common_configuration_args()
         args['value'] = self.get_value()
-        if self.has_changed(args, configuration, ['value']):
+        empty_value = args['value'] in [None, ''] and 'value' not in configuration
+        if self.has_changed(args, configuration, ['value']) and not empty_value:
             self.result['changed'] = True
             if not self.module.check_mode:
                 res = self.query_api('updateConfiguration', **args)
