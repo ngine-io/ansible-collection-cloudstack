4b09028d820209be9c38bc868f48e8ee6160a15e
rafael@valletech.eu <rafael@valletech.eu>
2020-12-11 10:42:13 +0100

    fixes #51, refactors for getting right exception message in all cases, refactors for reuse with inventory plugin

--- plugins/module_utils/cloudstack.py
+++ plugins/module_utils/cloudstack.py
@@ -43,6 +43,30 @@ def cs_required_together():
     return [['api_key', 'api_secret']]
 
 
+def cs_get_api_config(params):
+    api_config_error = ''
+    api_region = params.get('api_region') or os.environ.get('CLOUDSTACK_REGION')
+    try:
+        config = read_config(api_region)
+    except (KeyError, SystemExit, ValueError) as e:
+        api_config_error = str(e)
+        config = {}
+
+    api_config = {
+        'endpoint': params.get('api_url') or config.get('endpoint'),
+        'key': params.get('api_key') or config.get('key'),
+        'secret': params.get('api_secret') or config.get('secret'),
+        'timeout': params.get('api_timeout') or config.get('timeout') or 10,
+        'method': params.get('api_http_method') or config.get('method') or 'get',
+        'verify': params.get('api_verify_ssl_cert') or config.get('verify'),
+    }
+
+    if not all([api_config['endpoint'], api_config['key'], api_config['secret']]):
+        raise SystemExit("Missing api credentials: can not authenticate. " + api_config_error)
+
+    return api_config
+
+
 class AnsibleCloudStack:
 
     def __init__(self, module):
@@ -114,20 +138,12 @@ class AnsibleCloudStack:
         return self._cs
 
     def get_api_config(self):
-        api_region = self.module.params.get('api_region') or os.environ.get('CLOUDSTACK_REGION')
         try:
-            config = read_config(api_region)
-        except (KeyError, SystemExit):
-            config = {}
+            api_config = cs_get_api_config(self.module.params)
+        except CloudStackException as e:
+            self.fail_json(msg=str(e))
 
-        api_config = {
-            'endpoint': self.module.params.get('api_url') or config.get('endpoint'),
-            'key': self.module.params.get('api_key') or config.get('key'),
-            'secret': self.module.params.get('api_secret') or config.get('secret'),
-            'timeout': self.module.params.get('api_timeout') or config.get('timeout') or 10,
-            'method': self.module.params.get('api_http_method') or config.get('method') or 'get',
-            'verify': self.module.params.get('api_verify_ssl_cert') or config.get('verify'),
-        }
+        api_region = self.module.params.get('api_region') or os.environ.get('CLOUDSTACK_REGION')
         self.result.update({
             'api_region': api_region,
             'api_url': api_config['endpoint'],
@@ -136,8 +152,7 @@ class AnsibleCloudStack:
             'api_http_method': api_config['method'],
             'api_verify_ssl_cert': api_config['verify'],
         })
-        if not all([api_config['endpoint'], api_config['key'], api_config['secret']]):
-            self.fail_json(msg="Missing api credentials: can not authenticate")
+
         return api_config
 
     def fail_json(self, **kwargs):
