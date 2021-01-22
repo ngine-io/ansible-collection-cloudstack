bd380df81c189f1239f7d517af70130ee37e7b73
rafael@valletech.eu <rafael@valletech.eu>
2021-01-22 16:30:25 +0100

    fixing integration test after removing ini support

--- tests/integration/targets/inventory_cloudstack/runme.sh
+++ tests/integration/targets/inventory_cloudstack/runme.sh
@@ -5,13 +5,15 @@ env
 # Required to differentiate between Python 2 and 3 environ
 PYTHON=${ANSIBLE_TEST_PYTHON_INTERPRETER:-python}
 
-# TODO: the test environment is bit setup when running this integration test on its own.
+# TODO: the test environment is not setup when running this integration test on its own.
 ${PYTHON} -m pip install cs
 
-CLOUDSTACK_CONFIG=$(pwd)/cloudstack.ini
-export CLOUDSTACK_CONFIG
-
 # TODO: why is it looking for cloudstack conf section?
+ansible-playbook playbooks/basic-configuration.yml "$@"
+
+# Configure simulator endpoint
+source cloudstack.env
+
 ansible-playbook playbooks/cloudstack-inventory-test.yml "$@"
 
 ansible-inventory --list -i test.cloudstack.yml "$@"
