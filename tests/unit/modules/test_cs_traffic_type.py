13a1049f0f01c3872e3272eca3f8a2813450012b
Rene Moser <mail@renemoser.net>
2021-08-14 17:51:23 +0200

    fix sanity check

--- tests/unit/modules/test_cs_traffic_type.py
+++ tests/unit/modules/test_cs_traffic_type.py
@@ -1,7 +1,7 @@
 from __future__ import (absolute_import, division, print_function)
 import sys
 import pytest
-import units.compat.unittest as unittest
+from units.compat import unittest
 from units.compat.mock import MagicMock
 from units.compat.unittest import TestCase
 from units.modules.utils import set_module_args
