7614a8642c9e26ba20315afaec3fe6f3d85d810e
Lorenzo Tanganelli <lorenzo.tanganelli@hotmail.it>
2024-12-02 07:49:06 +0000

    add copyright lookup api

--- plugins/lookup/api.py
+++ plugins/lookup/api.py
@@ -1,4 +1,4 @@
-# (c) 2020 Ansible Project
+# Copyright (c) 2024, Lorenzo Tanganelli
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
 
